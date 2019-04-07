24ab9e860145c32ec7a1aa1ff3e87badcae9a927
zfleckhawk <zfleck@hawk.iit.edu>
2019-04-07 18:53:50 -0500

    Modify form a bit and add base Javascript

---
+++ site.js
@@ -0,0 +1,50 @@
+"use strict";
+
+(function() {
+  if (!('querySelector' in document && 'addEventListener' in document)) {
+    return;
+  }
+
+  function eq(value, condition) {
+    return value === condition;
+  }
+
+  function whitespace(value) {
+    console.log("The corrected email is " + value.replace(/\s/g, ''));
+    return value.replace(/\s/g, '');
+  }
+
+  function validate(value, check, condition) {
+    if (eq(typeof(check.test), 'function')) {
+      return check.test(value);
+    } else if (eq(typeof(check), 'function')) {
+      return check(value, condition);
+    } else {
+      return false;
+    }
+  }
+
+  function validate_email(value) {
+    var email = whitespace(value);
+    return validate(email, /^[^@\s]+@[^@\s]+.[^@\s]+$/g);
+}
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('#form');
+    var submit = document.querySelector('#submit');
+    var input_email = document.querySelector('#email');
+
+    submit.setAttribute('disabled', 'disabled');
+
+    form.addEventListener('keyup', function() {
+      if (validate_email(input_email.value)) {
+        submit.removeAttribute('disabled');
+        document.querySelector('#submit').style.cursor = "pointer";
+      } else {
+        submit.setAttribute('disabled', 'disabled');
+        document.querySelector('#submit').style.cursor = "default";
+      }
+});
+  });
+
+}());
