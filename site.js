374a2eedda2ba97d0bb7a40ca8e45d61af50f931
zfleckhawk <zfleck@hawk.iit.edu>
2019-04-17 21:34:47 -0500

    Added data validation across payment form

--- site.js
+++ site.js
@@ -10,7 +10,6 @@
   }
 
   function whitespace(value) {
-    console.log("The corrected email is " + value.replace(/\s/g, ''));
     return value.replace(/\s/g, '');
   }
 
@@ -29,16 +28,37 @@
     return validate(email, /^[^@\s]+@[^@\s]+.[^@\s]+$/g);
 }
 
+function validate_cc_num(value) {
+  var cc_num = whitespace(value);
+  return validate(cc_num, /^[0-9]{16}$/g)
+}
+
+function validate_code(value) {
+  var cvv = whitespace(value);
+  return validate(cvv, /^[0-9]{3}$/g)
+}
+
+function validate_expire(value) {
+  var date = whitespace(value);
+  return validate(date, /^[0-9]{6}$/g)
+}
+
+function validate_date(value) {
+  return validate(value, /^[0-9]{2}+-[0-9]{2}+-[0-9]{2}$/g)
+}
 
   document.addEventListener('DOMContentLoaded', function() {
     var form = document.querySelector('#pay-form');
     var submit = document.querySelector('#submit');
     var input_email = document.querySelector('#email');
+    var input_cc_num = document.querySelector('#credit');
+    var input_cvv = document.querySelector('#code');
+    var input_expire = document.querySelector('#expire');
 
     submit.setAttribute('disabled', 'disabled');
 
     form.addEventListener('keyup', function() {
-      if (validate_email(input_email.value)) {
+      if (validate_email(input_email.value)&&validate_cc_num(input_cc_num.value)&&validate_code(input_cvv.value)&&validate_expire(input_expire.value)) {
         submit.removeAttribute('disabled');
         document.querySelector('#submit').style.cursor = "pointer";
       } else {
